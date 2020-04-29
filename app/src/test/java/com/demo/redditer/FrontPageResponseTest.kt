9b3d86d514833ffc594b05ea516fe92c99d251a8
Andrej <a@net.hr>
2020-04-29 20:54:08 +0200

    added private modifier so AS doesn't complain;

--- app/src/test/java/com/demo/redditer/FrontPageResponseTest.kt
+++ app/src/test/java/com/demo/redditer/FrontPageResponseTest.kt
@@ -1,24 +1,22 @@
 package com.demo.redditer
 
 import android.content.Context
+import com.demo.redditer.data.api.ApiService
 import com.demo.redditer.koin.appModules
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
 import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import org.koin.test.KoinTest
 import org.koin.test.inject
 import org.mockito.Mockito.mock
 import org.mockito.MockitoAnnotations
-import com.demo.redditer.data.api.ApiDataSourceImpl
-import com.demo.redditer.data.api.ApiService
 
 // verify that front page response is returned and is correctly mapped
 class FrontPageResponseTest : KoinTest {
 
-    val apiService: ApiService by inject()
+    private val apiService: ApiService by inject()
 
     @Before
     fun setUp() {
